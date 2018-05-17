6c8fde7a1f8f7a46cf7e489c4270c0a770dfc876
Unknown <davisjuliaw@gmail.com>
2018-05-17 13:29:34 -0400

    Lesson 24 complete

--- src/TwentyFour.java
+++ src/TwentyFour.java
@@ -15,7 +15,34 @@ public class TwentyFour {
         {
             result = "Monday";
         }
-
+        else if ( weekday == 3 )
+        {
+            result = "Tuesday";
+        }
+        else if ( weekday == 4 )
+        {
+            result = "Wednesday";
+        }
+        else if ( weekday == 5 )
+        {
+            result = "Thursday";
+        }
+        else if ( weekday == 6 )
+        {
+            result = "Friday";
+        }
+        else if ( weekday == 7 )
+        {
+            result = "Saturday";
+        }
+        else if ( weekday == 0 )
+        {
+            result = "Sunday";
+        }
+        else
+            {
+            result = "ERROR";
+        }
         return result;
     }
 
